8d2d75eeb7b489db772757141d76741b2bf39aa6
shenshah <sarthak.shah@IB-SEZ-PC-208.infobeans.in>
2016-10-05 17:25:34 +0530

    sarthak change comitt 5:25 pm

--- Trial/src/trial.java
+++ Trial/src/trial.java
@@ -7,6 +7,8 @@ public class trial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("------ssssss---------");
+System.out.println("------ssssss---------");
+System.out.println("------ssssss---------");
 	}
 
 }
