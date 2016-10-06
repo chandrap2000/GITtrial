57820d6296993931b77136b6b9b846e58c23393a
chandrap2000 <chandrap2000@gmail.com>
2016-10-06 13:08:48 +0530

    Update NewTrial.java

--- Trial/src/NewTrial.java
+++ Trial/src/NewTrial.java
@@ -8,6 +8,7 @@ public class NewTrial {
 		// TODO Auto-generated method stub
 System.out.println("new created on thursday 11am");
 System.out.println("------ssssss---------");
+		System.out.println("------sss11111111111111111sss---------");
 	}
 
 }
